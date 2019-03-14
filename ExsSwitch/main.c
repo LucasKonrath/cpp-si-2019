d8b57aa721c9926317c0b7f0c284135a70224f0b
Lucas Konrath Damaceno <LUCASKD@edu.unisinos.br>
2019-03-13 21:32:23 -0300

    adicionar exs de switch

---
+++ ExsSwitch/main.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    int diaSemana;
+//
+//    printf("Informe o dia da semana de 1 a 7: \n");
+//    scanf("%d", &diaSemana);
+//
+//    switch (diaSemana)
+//    {
+//    case 1:
+//        printf("Domingo, FIM DE SEMANA");
+//        break;
+//    case 2:
+//        printf("Segunda, DIA UTIL");
+//        break;
+//    case 3:
+//        printf("Terça, DIA UTIL");
+//        break;
+//    case 4:
+//        printf("Quarta, DIA UTIL");
+//        break;
+//    case 5:
+//        printf("Quinta, DIA UTIL");
+//        break;
+//    case 6:
+//        printf("Sexta, DIA UTIL");
+//        break;
+//    case 7:
+//        printf("Sabado, fim de semana");
+//        break;
+//    default:
+//        printf("Dia invalido");
+//        break;
+
+//    }
+
+//   int mes;
+//
+//    printf("Informe o mes de 1 a 12: \n");
+//    scanf("%d", &mes);
+//
+//    switch (mes)
+//    {
+//    case 1:
+//        printf("Janeiro, 1o semestre, 31, verao");
+//        break;
+//    case 2:
+//        printf("Fevereiro, 1o semestre, 28, verao");
+//        break;
+//    case 3:
+//        printf("Março, 1o semestre, 31, verao");
+//        break;
+//    case 4:
+//        printf("Abril, 1o semestre, 30, outono");
+//        break;
+//    case 5:
+//        printf("Maio,  1o semestre, 31, outono");
+//        break;
+//    case 6:
+//        printf("Junho, 1o semestre, 30, outono");
+//        break;
+//    case 7:
+//        printf("Julho, 2o semestre, 30, inverno");
+//        break;
+//    case 8:
+//        printf("Agosto, 2o semestre, 30, inverno");
+//        break;
+//    case 9:
+//        printf("Setembro, 2o semestre, 30, inverno");
+//        break;
+//    case 10:
+//        printf("Outubro, 2o semestre, 30, primavera");
+//        break;
+//    case 11:
+//        printf("Novembro, 2o semestre, 30, primavera");
+//        break;
+//    case 12:
+//        printf("Dezembro, 2o semestre, 30, primavera");
+//        break;
+//    default:
+//        printf("mes invalido");
+//        break;
+//        }
+
+
+//    char sexo;
+//
+//    printf("Informe um sexo: m/f ou M/F \n");
+//    scanf("%c", &sexo);
+//
+//    switch (sexo){
+//
+//    case 'm':
+//    case 'M':
+//        printf("Masculino");
+//        break;
+//    case 'f':
+//    case 'F':
+//        printf("Feminino");
+//        break;
+//    default:
+//        printf("sexo invalido");
+//    }
+
+//    float num1, num2;
+//    char signal;
+//
+//    scanf("%f%c%f", &num1,  &signal, &num2);
+//
+//    switch (signal){
+//    case '+':
+//        printf("%.2f", num1 + num2);
+//        break;
+//    case '-':
+//        printf("%.2f", num1 - num2);
+//        break;
+//    case 'x':
+//        printf("%.2f", num1 * num2);
+//        break;
+//    case '/':
+//        printf("%.2f", num1 / num2);
+//        break;
+//    default:
+//        printf("input invalido");
+//        break;
+//    }
+
+//int codigo, quantidade;
+//printf("Digite o codigo e a quantidade: \n");
+//scanf("%d %d", &codigo, &quantidade);
+//
+//switch(codigo){
+//case 100:
+//    printf("preco %.2f", 5.50 * quantidade);
+//    break;
+//case 101:
+//    printf("preco %.2f", 6.20 * quantidade);
+//    break;
+//case 102:
+//    printf("preco %.2f", 7.80 * quantidade);
+//    break;
+//case 103:
+//    printf("preco %.2f", 5.90 * quantidade);
+//    break;
+//case 104:
+//    printf("preco %.2f", 6.80 * quantidade);
+//    break;
+//case 105:
+//    printf("preco %.2f", 3.70 * quantidade);
+//    break;
+//case 106:
+//    printf("preco %.2f", 4.25 * quantidade);
+//    break;
+//default:
+//    printf("codigo invalido");
+//    break;
+//}
+}
